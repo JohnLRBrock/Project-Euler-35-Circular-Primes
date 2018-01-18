a322d0309d82fd2f1bfc0c26151479d42d346abc
John Brock <johnlrbrock@gmail.com>
2018-01-18 18:34:42 -0500

    initial commit

---
+++ circular_primes.rb
@@ -0,0 +1,18 @@
+# John Brock
+# 2018-01-18
+# https://projecteuler.net/problem=35
+# 
+# Circular Prime: A prime who's every rotation is also prime
+# Example: 197, 971, 719
+# how many circular primes are there under 1 million?
+
+
+def circular_primes()
+  return false
+end
+
+start = Time.now
+prime_count = circular_primes()
+finish = Time.now
+puts prime_count
+puts finish - start
